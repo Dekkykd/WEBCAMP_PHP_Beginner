9bb32bc415b69522db57c3bd4d2cdd4e84181e77
hideki <hideki@example.com>
2025-12-24 03:48:23 +0000

    型関連の学習用のコードを書いた

---
+++ type_string.php
@@ -0,0 +1,19 @@
+<?php
+// 文字列型の確認
+$s = "Hello";
+var_dump($s);
+
+// 1文字目にアクセス
+echo $s[1] , "\n";
+
+// 「数字」と「数値」
+$num_i = 123;
+$num_s = "123";
+var_dump($num_i);
+var_dump($num_s);
+
+// シングルクォートとダブルクォートの違い
+$double_s = "num is {$num_i} \n";
+$single_s = 'num is {$num_i} \n';
+var_dump($double_s);
+var_dump($single_s);
